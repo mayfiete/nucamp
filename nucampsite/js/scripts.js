d85cb09660f68b8a791eb7d44a31561068ba6000
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-10-14 09:09:49 -0400

    feat: adding sassy css and javascript for workshop

--- nucampsite/js/scripts.js
+++ nucampsite/js/scripts.js
@@ -13,3 +13,28 @@ $(function () {
     }
   });
 });
+
+/* 
+Add JavaScript (in the form of jQuery) to the scripts.js file that will 
+activate the Reserve Campsite and Login modals when the 
+corresponding button is clicked.
+*/
+
+
+$(function () {
+  // Attach Button click event listener 
+ $("#reserveButton").click(function(){
+
+      // show Modal
+      $('#reservecsModal').modal('show');
+ });
+});
+
+$(function () {
+  // Attach Button click event listener 
+ $("#loginButton").click(function(){
+
+      // show Modal
+      $('#loginModal').modal('show');
+ });
+});
